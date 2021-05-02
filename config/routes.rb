c6642b1f5f96fe07040b4966165ed4b47863c2ed
Aleh13 <q4wk13@gmail.com>
2021-05-02 11:04:42 +0300

    создание предсдавления new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'semesters/new'
-  get 'semester/new'
-  #resources :semesters, shallow: true do
+  #get 'semesters/new'
+  #get 'semester/new'
+  resources :semesters, shallow: true do
   #resources :courses
+end
 end
