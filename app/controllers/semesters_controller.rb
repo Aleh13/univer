9c9e7e2124c7146de9448a57957af77be7ba6915
Aleh13 <q4wk13@gmail.com>
2021-05-02 22:53:42 +0300

    создаем эндпоинт и представление show

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,5 +1,33 @@
 class SemestersController < ApplicationController
-  def new
-  	@semester = Semester.new
-  end
+
+ def show
+  @semester = Semester.find(params[:id])
+ end
+
+
+
+def new
+  @semester = Semester.new
+end
+
+
+def create
+  @semester = Semester.create(semester_params)
+    if @semester.errors.empty?
+      redirect_to @semester
+    else
+      render 'new'
+    end
+end
+
+
+
+
+private
+ def semester_params
+   params.require(:semester).permit(:name, :fullname, :current)
+end
+
+
+
 end
